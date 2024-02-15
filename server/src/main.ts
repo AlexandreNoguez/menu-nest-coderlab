ccf4fcb35aab804058c5acfc36620dbae6866384
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-02-15 00:52:36 -0300

    fix: added log with health msg, port and date when started

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.API_LOCAL_PORT;
-  console.log(port);
-  await app.listen(port);
+  const port = process.env.API_LOCAL_PORT || 3333;
+  await app.listen(port, () => {
+    console.log(`Server started at port ${port} ${new Date().toLocaleString("pt-br")}`);
+  });
 
 }
 bootstrap();
