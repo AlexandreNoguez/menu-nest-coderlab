d88b370dddc51f4e4cb893cc6a9b3ca33051378b
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-02-15 02:46:19 -0300

    fix: .env usage

--- server/db/data-source.ts
+++ server/db/data-source.ts
@@ -2,12 +2,11 @@ import { DataSource, DataSourceOptions } from "typeorm";
 
 export const dataSourceOptions: DataSourceOptions = {
     type: 'mysql',
-    host: 'localhost',
-    port: 3307,
-    username: 'root',
-    password: 'root',
-    database: 'menu-db',
-    synchronize: false,
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_LOCAL_PORT),
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     entities: ['dist/**/*.entity.js'],
     migrations: ['dist/db/migrations/*.js'],
     migrationsTableName: "menu_app_migrations"
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,20 +5,29 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env'
+      isGlobal: true,
+      envFilePath: ".env"
     }),
     TypeOrmModule.forRoot(typeOrmConfig),
     UsersModule,
     ProductsModule,
-    AuthModule
+    AuthModule,
+    CategoriesModule
   ],
 })
 export class AppModule {
 
 }
+
+// console.log(process.env.MYSQL_HOST);
+// console.log(process.env.MYSQL_LOCAL_PORT);
+// console.log(process.env.MYSQL_USERNAME);
+// console.log(process.env.MYSQL_PASSWORD);
+// console.log(process.env.MYSQL_DATABASE);
 
