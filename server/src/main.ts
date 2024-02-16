1bded330a5bda20202a7378f282257cf9dc47c0d
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-02-16 13:15:30 -0300

    fix: enabled cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.API_LOCAL_PORT || 3333;
+
+  app.enableCors(); // I'll let cors open to make easier to test
+
   await app.listen(port, () => {
     console.log(`Server started at port ${port} ${new Date().toLocaleString("pt-br")}`);
   });
