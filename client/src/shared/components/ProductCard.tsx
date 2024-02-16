1a845a96247759a2ae848e6ba95ff87b27a6bf2a
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-02-16 16:44:22 -0300

    fix: Removed some info and added BRL cost

--- client/src/shared/components/ProductCard.tsx
+++ client/src/shared/components/ProductCard.tsx
@@ -2,12 +2,12 @@ import { IProduct } from "../types/IProduct"
 
 export const ProductCard = ({ photo, name, qty, price, category }: IProduct) => {
     return (
-        <div className="border border-gray-300 rounded-lg p-4 shadow-md w-64 m-4">
-            <img src={photo} alt={name} className="rounded-lg mb-4" />
+        <div className="border border-gray-300 rounded-lg p-4 shadow-md w-64 h-52 m-4">
+            <img src={photo} alt={name} className="rounded-lg mb-4 max-w-[120px] h-24 mx-auto" />
             <h2 className="text-lg font-semibold">{name}</h2>
-            <p className="text-sm text-gray-600">Quantity: {qty}</p>
-            <p className="text-sm text-gray-600">Price: ${price}</p>
-            <p className="text-sm text-gray-600">Category: {category}</p>
+            {/* <p className="text-sm text-gray-600">Quantity: {qty}</p> */}
+            <p className="text-sm text-gray-600">Preço: R${price}</p>
+            {/* <p className="text-sm text-gray-600">Category: {category}</p> */}
         </div>
     )
 }
