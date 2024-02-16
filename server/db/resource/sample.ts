eebd3b5bfa8df2ff30e8e68a8f5df65b95e43ff7
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-02-16 16:43:31 -0300

    fix: added url photo to products

--- server/db/resource/sample.ts
+++ server/db/resource/sample.ts
@@ -104,104 +104,104 @@ export const products = [
         name: "Filé Mignon",
         qty: 10,
         price: 29.99,
-        photo: "file_mignon.jpg"
+        photo: "https://images.pexels.com/photos/1307658/pexels-photo-1307658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 7,
         name: "Costela de Porco",
         qty: 15,
         price: 24.99,
-        photo: "costela_porco.jpg"
+        photo: "https://images.pexels.com/photos/410648/pexels-photo-410648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 8,
         name: "Frango Assado",
         qty: 20,
         price: 19.99,
-        photo: "frango_assado.jpg"
+        photo: "https://images.pexels.com/photos/5718025/pexels-photo-5718025.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 8,
         name: "Frango Grelhado",
         qty: 18,
         price: 17.99,
-        photo: "frango_grelhado.jpg"
+        photo: "https://images.pexels.com/photos/262945/pexels-photo-262945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 9,
         name: "Salmão Grelhado",
         qty: 12,
         price: 27.99,
-        photo: "salmao_grelhado.jpg"
+        photo: "https://images.pexels.com/photos/725991/pexels-photo-725991.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 10,
         name: "Arroz Branco",
         qty: 30,
         price: 4.99,
-        photo: "arroz_branco.jpg"
+        photo: "https://images.pexels.com/photos/8956718/pexels-photo-8956718.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 10,
         name: "Feijão Carioca",
         qty: 25,
         price: 5.99,
-        photo: "feijao_carioca.jpg"
+        photo: "https://vitat.com.br/alimentacao/busca-de-alimentos/Content/img/alimentos/1759-feij%C3%A3o-carioca-cozido.jpg"
     },
     {
         category: 11,
         name: "Coca-Cola",
         qty: 50,
         price: 2.99,
-        photo: "coca_cola.jpg"
+        photo: "https://images.pexels.com/photos/4389665/pexels-photo-4389665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 11,
         name: "Suco de Laranja",
         qty: 40,
         price: 3.99,
-        photo: "suco_laranja.jpg"
+        photo: "https://images.pexels.com/photos/158053/fresh-orange-juice-squeezed-refreshing-citrus-158053.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 12,
         name: "Bolo de Chocolate",
         qty: 15,
         price: 6.99,
-        photo: "bolo_chocolate.jpg"
+        photo: "https://images.pexels.com/photos/2957897/pexels-photo-2957897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 12,
         name: "Sorvete de Baunilha",
         qty: 20,
         price: 3.99,
-        photo: "sorvete_baunilha.jpg"
+        photo: "https://images.pexels.com/photos/1294943/pexels-photo-1294943.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 13,
         name: "Tábua de Frios",
         qty: 10,
         price: 12.99,
-        photo: "tabua_frios.jpg"
+        photo: "https://delicious.com.br/wp-content/uploads/2020/10/tabua-de-frios-delicious.png"
     },
     {
         category: 13,
         name: "Bruschetta de Tomate",
         qty: 14,
         price: 8.99,
-        photo: "bruschetta_tomate.jpg"
+        photo: "https://images.pexels.com/photos/11218690/pexels-photo-11218690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 14,
         name: "Hambúrguer Kids",
         qty: 20,
         price: 7.99,
-        photo: "hamburguer_kids.jpg"
+        photo: "https://images.pexels.com/photos/13785147/pexels-photo-13785147.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         category: 14,
         name: "Sorvete de Chocolate",
         qty: 25,
         price: 2.99,
-        photo: "sorvete_chocolate.jpg"
+        photo: "https://images.pexels.com/photos/126790/pexels-photo-126790.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     }
 ]
